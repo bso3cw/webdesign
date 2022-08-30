f4299fb36994760cf39631c7e564084bdd44f97e
bso3cw <bso3cw@naver.com>
2022-08-30 16:49:27 +0900

    ㄹ

--- behr/js/main.js
+++ behr/js/main.js
@@ -21,18 +21,22 @@ $(document).ready(function(){
        조건 2 - 스크롤 값이 0이면 header에 fixed 클래스 삭제 */
 
     /* 로딩했을 때 맨 처음 */
-    let scrolling=$(window).scrollTop()
-    console.log(scrolling)
+    let scrolling
+    headerFixed() // 함수의 실행
 
+    
     /* 스크롤 할 때마다 실행 */
     $(window).scroll(function(){
+        headerFixed() // 함수의 실행
+    })
+
+    function headerFixed(){ // 함수의 선언
         scrolling=$(window).scrollTop()
-        console.log(scrolling)
-        if(scrolling>0){
+        if(scrolling > 0){
             $('header').addClass('fixed')
         }else{
             $('header').removeClass('fixed')
         }
-    })
+    }
 })
 
