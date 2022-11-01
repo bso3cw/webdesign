e3aa0383ec875b80fb1538fb0a997f3cf3d08152
bso3cw <bso3cw@naver.com>
2022-11-01 18:00:33 +0900

    .

--- bntailor/js/common.js
+++ bntailor/js/common.js
@@ -22,4 +22,31 @@ $(document).ready(function(){
             $('.header').removeClass('fixed')
         }
     }
+
+    /* 모바일 메뉴 열기
+       .header .gnb .gnb_open을 클릭했을 때
+       1. .header .gnb한테 mobile_open class를 추가하거나 삭제
+          (닫는 버튼과 여튼 버튼 두 가지 역할을 모두 함)
+       2. .header .gnb .gnb_open strong에 쓰여 있는 글자를 메뉴 열기, 메뉴 닫기로 변경해줘야 함
+
+       메뉴가 열려 있는 상태인지 메뉴가 닫혀 있는 상태인지 구분
+       닫혀 있으면 mobile_open class 추가, "메뉴 닫기"로 문구 변경
+       열려 있으면 mobile_open class 삭제, "메뉴 열기"로 문구 변경
+
+       mobile_open class가 없으면 닫혀 있는 상태 / 있으면 열려 있는 상태 */
+
+    let gnbStu // true (열린 상태), false (닫힌 상태)
+    $('.header .gnb .gnb_open').on('click', function(){
+        gnbStu = $('.header .gnb').hasClass('mobile_open')
+        if(gnbStu == true){ // 열린 상태 - 닫는 기능
+            // console.log('true 열린 상태')
+            $('.header .gnb').removeClass('mobile_open')
+            $('.header .gnb .gnb_open strong').text('메뉴 열기')
+        }else{ // 닫힌 상태 - 여는 기능
+            // console.log('false 닫힌 상태')
+            $('.header .gnb').addClass('mobile_open')
+            $('.header .gnb .gnb_open strong').text('메뉴 닫기')
+        }
+    })
+
 })
