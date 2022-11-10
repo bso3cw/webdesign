829e3e924f82b7b1d8f30f1c4f745b71553e41e1
bso3cw <bso3cw@naver.com>
2022-11-10 18:01:27 +0900

    포트폴리오 작업

--- lotteria/js/main.js
+++ lotteria/js/main.js
@@ -17,6 +17,10 @@ $(document).ready(function(){
     })
 
 
+    $('.cnts .list li h2').on('click',function(){
+        $('.cnts ul.list>li').removeClass('active')
+        $(this).parent().addClass('active')
+    })
 
 
 
