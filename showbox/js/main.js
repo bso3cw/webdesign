c9d65ee48bd2bfb21021c1c441538d4d9ab23551
bso3cw <bso3cw@naver.com>
2022-11-17 18:01:02 +0900

    포트폴리오

--- showbox/js/main.js
+++ showbox/js/main.js
@@ -4,8 +4,8 @@ $(document).ready(function(){
 
     navigation: true, /* 오른쪽에 각 페이지의 paging */
     navigationPosition: 'left', /* 위치 */
-    navigationTooltips: ['1', '2', '3', '4'], /* 툴팁 */
-    showActiveTooltip: false, /* 현재 활성화된 페이지의 툴팁에 특정 클래스 주기 */
+    navigationTooltips: ['1','<strong>Production/Investment</strong> 영화 제작/투자', '<strong>Film Distribution</strong> 국내/해외영화 배급', '<strong>Advertisement/Marketing</strong> 홍보/마케팅', '<strong>Overseas Theater Business</strong> 해외 영화관 사업'], /* 툴팁 */
+    showActiveTooltip: true, /* 현재 활성화된 페이지의 툴팁에 특정 클래스 주기 */
 
     autoScrolling:true, /* 한페이지씩 스크롤 */
     scrollHorizontally: true,
@@ -13,8 +13,15 @@ $(document).ready(function(){
     verticalCentered: true, /* 컨텐츠 요소 위아래 가운데 */
 
     afterLoad: function(origin, destination, direction, trigger){
-        if(destination.index == 2){ /* index가 2면 슬라이드는 세번째 슬라이드입니다. index 수는 0/1/2/3 */
-            console.log('3번째 슬라이드가 로딩 되었을때');
+        if((destination.index == 1)||(destination.index == 2)||(destination.index == 3)||(destination.index == 4)){ /* index가 2면 슬라이드는 세번째 슬라이드입니다. index 수는 0/1/2/3 */
+            $('.header').addClass('fixed')
+        }else{
+            $('.header').removeClass('fixed')
+        }
+        if(destination.index == 0){
+            $('#fp-nav').hide()
+        }else{
+            $('#fp-nav').show()
         }
     },
 
